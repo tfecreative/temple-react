ca4ce6d274cdcaa6dc1d1d3ff0d39b0be11f7b72
tfe <tfecreative@gmail.com>
2020-08-19 17:50:31 -0400

    Add account service
    Add account service with sign in request

---
+++ src/Services/Account/Account.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from "Services/Http/HttpClient";
+import { SignInRequest, SignInResponse } from "./Types";
+
+export class AccountService extends HttpClient {
+  public async signIn(credentials: SignInRequest): Promise<SignInResponse> {
+    return await this.client
+      .post<SignInResponse>("auth/sign-in/", credentials)
+      .then(({ data }) => data);
+  }
+}
+
+export const accountService = new AccountService();
---
+++ src/Services/Account/index.ts
@@ -0,0 +1,2 @@
+export * from "Services/Account/Account";
+export * from "Services/Account/Types";
