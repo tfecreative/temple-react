0ea4691580321dbcb5a7f6e20b7a7363f0b4c22d
tfe <tfecreative@gmail.com>
2020-08-20 20:29:48 -0400

    Use store in sign in form
    Update sign in form to use store

--- src/modules/SignIn/components/SignInForm/SignInForm.tsx
+++ src/modules/SignIn/components/SignInForm/SignInForm.tsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./SignInForm.scss";
 import { Form } from "components/Form/Form";
 import { FormHeader } from "components/Form/FormHeader";
 import { FormInput } from "components/FormInput/FormInput";
 import { FormButton } from "components/Form/FormButton";
-import { accountService, SignInRequest } from "services/Account";
 import { toast } from "react-toastify";
+import { connect } from "react-redux";
+import { signIn } from "store/user/action";
+import { User, UserState } from "store/user/types";
+import { ApplicationState } from "store";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
 
-const SignInForm = () => {
-  const handleSubmit = async (values: any) => {
+export interface SignInFormProps {
+  user: User,
+  signIn: (username: string, password: string) => any;
+}
+
+const SignInForm: React.FC<SignInFormProps> = (props: SignInFormProps) => {
+  const handleSubmit = async(values: any) => {
     try {
-      const req: SignInRequest = {
-        username: values.username,
-        password: values.password,
-      };
-      await accountService.signIn(req);
+      await props.signIn(values.username, values.password);
+      // await this.SignIn(values.username, values.password);
     } catch (e) {
       toast.error(e.message);
     }
-  };
+  }
 
   return (
     <Form onSubmit={handleSubmit} className="sign-in-form">
       <FormHeader text="Sign In"></FormHeader>
+      <div>{props.user.token ?? "???"}</div>
       <FormInput name="username" label="Username" />
       <FormInput name="password" label="Password" />
       <div className="form-actions">
@@ -30,6 +38,17 @@ const SignInForm = () => {
       </div>
     </Form>
   );
+}
+
+const mapStateToProps = ({ userState }: ApplicationState) => ({
+  user: userState.user
+});
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, AnyAction>) => {
+  return {
+    signIn: (username: string, password: string) => dispatch(signIn(username, password))
+  };
 };
 
-export default SignInForm;
+const connected = connect(mapStateToProps, mapDispatchToProps)(SignInForm);
+export default connected;
