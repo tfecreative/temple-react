709117a395b08eae189f150dcf4c6277dfa34e9a
tfe <tfecreative@gmail.com>
2020-08-24 12:33:18 -0400

    Use hook
    Replace use of connect with hook

--- src/modules/SignIn/components/SignInForm/SignInForm.tsx
+++ src/modules/SignIn/components/SignInForm/SignInForm.tsx
@@ -5,22 +5,16 @@ import { FormHeader } from "components/Form/FormHeader";
 import { FormInput } from "components/FormInput/FormInput";
 import { FormButton } from "components/Form/FormButton";
 import { toast } from "react-toastify";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import { signIn } from "store/auth/action";
-import { Auth } from "store/auth/types";
-import { ApplicationState } from "store";
-import { ThunkDispatch } from "redux-thunk";
-import { AnyAction } from "redux";
 
-export interface SignInFormProps {
-  auth: Auth;
-  signIn: (username: string, password: string) => any;
-}
+const SignInForm = () => {
+  const dispatch = useDispatch();
 
-const SignInForm: React.FC<SignInFormProps> = (props: SignInFormProps) => {
-  const handleSubmit = async (values: any) => {
+  const handleSubmit = (values: any) => {
     try {
-      await props.signIn(values.username, values.password);
+      const payload = { username: values.username, password: values.password };
+      dispatch(signIn(payload));
     } catch (e) {
       toast.error(e.message);
     }
@@ -29,7 +23,6 @@ const SignInForm: React.FC<SignInFormProps> = (props: SignInFormProps) => {
   return (
     <Form onSubmit={handleSubmit} className="sign-in-form">
       <FormHeader text="Sign In"></FormHeader>
-      <div>{props.auth.token ?? "???"}</div>
       <FormInput name="username" label="Username" />
       <FormInput name="password" label="Password" />
       <div className="form-actions">
@@ -39,16 +32,4 @@ const SignInForm: React.FC<SignInFormProps> = (props: SignInFormProps) => {
   );
 };
 
-const mapStateToProps = ({ auth }: ApplicationState) => ({
-  auth: auth.auth,
-});
-
-const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, AnyAction>) => {
-  return {
-    signIn: (username: string, password: string) =>
-      dispatch(signIn(username, password)),
-  };
-};
-
-const connected = connect(mapStateToProps, mapDispatchToProps)(SignInForm);
-export default connected;
+export default SignInForm;
