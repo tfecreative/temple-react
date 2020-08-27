a7590bf9ed437a32d5f435e912fe706d920a9b1e
tfe <tfecreative@gmail.com>
2020-08-27 09:50:33 -0400

    Refactor account service

--- src/services/Account/Account.ts
+++ src/services/Account/Account.ts
@@ -1,17 +1,13 @@
 import { HttpClient } from "services/Http/HttpClient";
-import { SignInRequest, SignInResponse } from "./Types";
-import { SignUpRequest, SignUpResponse } from "models/sign-up";
+import { SignInResponse } from "./Types";
 
 export class AccountService extends HttpClient {
-  public async signIn(credentials: SignInRequest): Promise<SignInResponse> {
+  public async signIn(
+    username: string,
+    password: string
+  ): Promise<SignInResponse> {
     return await this.client
-      .post<SignInResponse>("auth/sign-in/", credentials)
-      .then(({ data }) => data);
-  }
-
-  public async signUp(payload: SignUpRequest): Promise<SignUpResponse> {
-    return await this.client
-      .post<SignUpResponse>("auth/sign-up/", payload)
+      .post<SignInResponse>("auth/sign-in/", { username, password })
       .then(({ data }) => data);
   }
 }
