fe099bd7acf4b31d824a7dff1238615b4684ee91
tfe <tfecreative@gmail.com>
2020-09-13 15:26:19 -0400

    Include avatar menu in app header
    Style updates

--- src/modules/Layout/Header/Header.tsx
+++ src/modules/Layout/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Header.scss";
+import { AvatarMenu } from "modules/Account/AvatarMenu";
 
 function Header() {
   return (
     <header className="app-header">
-      <span className="app-title">temple</span>
+      <div className="app-header__title">temple</div>
+      <div className="app-header__actions">
+        <AvatarMenu></AvatarMenu>
+      </div>
     </header>
   );
 }
