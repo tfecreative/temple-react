510ef9a1a7e7f3ff63e0b9ecc6badaf86c510335
tfe <tfecreative@gmail.com>
2020-09-14 14:33:16 -0400

    Add breakpoints for base app styles
    Set breakpoints for responsive styles

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App, { MainProps } from "./App";
-import configureStore from "store/store.config";
-import { createMemoryHistory } from "history";
-
-const history = createMemoryHistory();
-const initialState: any = {};
-const store = configureStore(history, initialState);
+import { store, history } from "store";
 
 const props: MainProps = {
   store,
